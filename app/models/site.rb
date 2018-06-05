f91a157445466e03b894db9184e7ae8018e0f945
ArtOfCode- <hello@artofcode.co.uk>
2018-06-05 14:39:40 +0100

    Names

--- app/models/site.rb
+++ app/models/site.rb
@@ -11,12 +11,12 @@ class Site < ApplicationRecord
     return unless sites.count > 100 # all is not well; bail
     sites.each do |site|
       uri = URI.parse(site['site_url'])
-      s = Site.find_or_initialize_by(site_domain: uri.host)
+      s = Site.find_or_initialize_by(domain: uri.host)
 
       s.api_parameter = site['api_site_parameter']
-      s.site_url = site['site_url']
-      s.site_logo = site['favicon_url'].gsub(/https?:/, '')
-      s.site_name = site['name']
+      s.url = site['site_url']
+      s.icon_url = site['favicon_url'].gsub(/https?:/, '')
+      s.name = site['name']
       s.is_child_meta = site['site_type'] == 'meta_site'
       s.save!
     end
